7c392354a5f67ef0d5fc628fb5417f2a2000ca4c
Arman-Aghajanyan <119808906+Arman-Aghajanyan@users.noreply.github.com>
2023-09-15 18:05:24 +0400

    Add files via upload

---
+++ assets/HomeComp/BigTitle.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function BigTitle() {
+  return (
+    <div className="BigTitle">
+
+      <div className="houseInfo">
+        <div>Coffee House</div>
+        <div>By Arman</div>
+      </div>
+    </div>
+  );
+}
+
+export default BigTitle;
